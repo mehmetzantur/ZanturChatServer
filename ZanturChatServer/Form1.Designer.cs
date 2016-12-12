300e5ae9b2264e45feb86f8ee4f7824b4fcbf8c0
mehmet zantur <mehmetzantur@gmail.com>
2016-12-12 20:09:10 +0300

    Add project files.

---
+++ ZanturChatServer/Form1.Designer.cs
@@ -0,0 +1,299 @@
+﻿namespace ZanturChatServer
+{
+    partial class frmServer
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pnlChat = new System.Windows.Forms.Panel();
+            this.pnlSendButtons = new System.Windows.Forms.Panel();
+            this.btnSend = new System.Windows.Forms.Button();
+            this.btnSendAll = new System.Windows.Forms.Button();
+            this.txtChat = new System.Windows.Forms.TextBox();
+            this.pnlMessages = new System.Windows.Forms.Panel();
+            this.txtMessages = new System.Windows.Forms.TextBox();
+            this.pnlRight = new System.Windows.Forms.Panel();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.button1 = new System.Windows.Forms.Button();
+            this.btnUserKick = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.txtPort = new System.Windows.Forms.TextBox();
+            this.txtIp = new System.Windows.Forms.TextBox();
+            this.pnlChat.SuspendLayout();
+            this.pnlSendButtons.SuspendLayout();
+            this.pnlMessages.SuspendLayout();
+            this.pnlRight.SuspendLayout();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pnlChat
+            // 
+            this.pnlChat.BackColor = System.Drawing.SystemColors.Window;
+            this.pnlChat.Controls.Add(this.pnlSendButtons);
+            this.pnlChat.Controls.Add(this.txtChat);
+            this.pnlChat.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.pnlChat.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.pnlChat.Location = new System.Drawing.Point(0, 427);
+            this.pnlChat.Name = "pnlChat";
+            this.pnlChat.Size = new System.Drawing.Size(759, 51);
+            this.pnlChat.TabIndex = 4;
+            // 
+            // pnlSendButtons
+            // 
+            this.pnlSendButtons.Controls.Add(this.btnSend);
+            this.pnlSendButtons.Controls.Add(this.btnSendAll);
+            this.pnlSendButtons.Dock = System.Windows.Forms.DockStyle.Right;
+            this.pnlSendButtons.Location = new System.Drawing.Point(565, 0);
+            this.pnlSendButtons.Name = "pnlSendButtons";
+            this.pnlSendButtons.Size = new System.Drawing.Size(194, 51);
+            this.pnlSendButtons.TabIndex = 7;
+            // 
+            // btnSend
+            // 
+            this.btnSend.Dock = System.Windows.Forms.DockStyle.Right;
+            this.btnSend.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnSend.Location = new System.Drawing.Point(0, 0);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(87, 51);
+            this.btnSend.TabIndex = 10;
+            this.btnSend.Text = "Send";
+            this.btnSend.UseVisualStyleBackColor = true;
+            // 
+            // btnSendAll
+            // 
+            this.btnSendAll.Dock = System.Windows.Forms.DockStyle.Right;
+            this.btnSendAll.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnSendAll.Location = new System.Drawing.Point(87, 0);
+            this.btnSendAll.Name = "btnSendAll";
+            this.btnSendAll.Size = new System.Drawing.Size(107, 51);
+            this.btnSendAll.TabIndex = 9;
+            this.btnSendAll.Text = "Send All";
+            this.btnSendAll.UseVisualStyleBackColor = true;
+            this.btnSendAll.Click += new System.EventHandler(this.btnSendAll_Click);
+            // 
+            // txtChat
+            // 
+            this.txtChat.Dock = System.Windows.Forms.DockStyle.Left;
+            this.txtChat.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtChat.Location = new System.Drawing.Point(0, 0);
+            this.txtChat.Multiline = true;
+            this.txtChat.Name = "txtChat";
+            this.txtChat.Size = new System.Drawing.Size(563, 51);
+            this.txtChat.TabIndex = 6;
+            this.txtChat.Text = "Chat...";
+            // 
+            // pnlMessages
+            // 
+            this.pnlMessages.Controls.Add(this.txtMessages);
+            this.pnlMessages.Dock = System.Windows.Forms.DockStyle.Left;
+            this.pnlMessages.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.pnlMessages.Location = new System.Drawing.Point(0, 0);
+            this.pnlMessages.Name = "pnlMessages";
+            this.pnlMessages.Size = new System.Drawing.Size(563, 427);
+            this.pnlMessages.TabIndex = 5;
+            // 
+            // txtMessages
+            // 
+            this.txtMessages.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.txtMessages.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtMessages.Location = new System.Drawing.Point(0, 0);
+            this.txtMessages.Multiline = true;
+            this.txtMessages.Name = "txtMessages";
+            this.txtMessages.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtMessages.Size = new System.Drawing.Size(563, 427);
+            this.txtMessages.TabIndex = 4;
+            // 
+            // pnlRight
+            // 
+            this.pnlRight.Controls.Add(this.tabControl1);
+            this.pnlRight.Dock = System.Windows.Forms.DockStyle.Right;
+            this.pnlRight.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.pnlRight.Location = new System.Drawing.Point(565, 0);
+            this.pnlRight.Name = "pnlRight";
+            this.pnlRight.Size = new System.Drawing.Size(194, 427);
+            this.pnlRight.TabIndex = 6;
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.tabControl1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.tabControl1.Location = new System.Drawing.Point(0, 0);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(194, 427);
+            this.tabControl1.TabIndex = 3;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.button1);
+            this.tabPage1.Controls.Add(this.btnUserKick);
+            this.tabPage1.Controls.Add(this.listBox1);
+            this.tabPage1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.tabPage1.Location = new System.Drawing.Point(4, 25);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(186, 398);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Online Users";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.button1.Location = new System.Drawing.Point(3, 354);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(180, 41);
+            this.button1.TabIndex = 10;
+            this.button1.Text = "Private Message";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnUserKick
+            // 
+            this.btnUserKick.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnUserKick.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnUserKick.Location = new System.Drawing.Point(3, 307);
+            this.btnUserKick.Name = "btnUserKick";
+            this.btnUserKick.Size = new System.Drawing.Size(180, 41);
+            this.btnUserKick.TabIndex = 9;
+            this.btnUserKick.Text = "User Kick";
+            this.btnUserKick.UseVisualStyleBackColor = true;
+            this.btnUserKick.Click += new System.EventHandler(this.btnUserKick_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.listBox1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.listBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(3, 3);
+            this.listBox1.Margin = new System.Windows.Forms.Padding(0);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(180, 304);
+            this.listBox1.TabIndex = 2;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.btnStart);
+            this.tabPage2.Controls.Add(this.txtPort);
+            this.tabPage2.Controls.Add(this.txtIp);
+            this.tabPage2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.tabPage2.Location = new System.Drawing.Point(4, 25);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(186, 398);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Connection";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // btnStart
+            // 
+            this.btnStart.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnStart.Location = new System.Drawing.Point(6, 58);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(176, 40);
+            this.btnStart.TabIndex = 2;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // txtPort
+            // 
+            this.txtPort.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtPort.Location = new System.Drawing.Point(6, 32);
+            this.txtPort.Name = "txtPort";
+            this.txtPort.Size = new System.Drawing.Size(176, 22);
+            this.txtPort.TabIndex = 1;
+            this.txtPort.Text = "8000";
+            // 
+            // txtIp
+            // 
+            this.txtIp.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtIp.Location = new System.Drawing.Point(6, 6);
+            this.txtIp.Name = "txtIp";
+            this.txtIp.Size = new System.Drawing.Size(176, 22);
+            this.txtIp.TabIndex = 0;
+            this.txtIp.Text = "127.0.0.1";
+            // 
+            // frmServer
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(759, 478);
+            this.Controls.Add(this.pnlRight);
+            this.Controls.Add(this.pnlMessages);
+            this.Controls.Add(this.pnlChat);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "frmServer";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "mzChat Server";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.frmServer_FormClosing);
+            this.Load += new System.EventHandler(this.frmServer_Load);
+            this.pnlChat.ResumeLayout(false);
+            this.pnlChat.PerformLayout();
+            this.pnlSendButtons.ResumeLayout(false);
+            this.pnlMessages.ResumeLayout(false);
+            this.pnlMessages.PerformLayout();
+            this.pnlRight.ResumeLayout(false);
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Panel pnlChat;
+        private System.Windows.Forms.TextBox txtChat;
+        private System.Windows.Forms.Panel pnlMessages;
+        private System.Windows.Forms.TextBox txtMessages;
+        private System.Windows.Forms.Panel pnlRight;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.TextBox txtPort;
+        private System.Windows.Forms.TextBox txtIp;
+        private System.Windows.Forms.Button btnUserKick;
+        private System.Windows.Forms.Panel pnlSendButtons;
+        private System.Windows.Forms.Button btnSend;
+        private System.Windows.Forms.Button btnSendAll;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ ZanturChatServer/Form1.cs
@@ -0,0 +1,262 @@
+﻿using NetworksApi.TCP.SERVER;
+using System.Text;
+using System.Windows.Forms;
+
+namespace ZanturChatServer
+{
+
+    public delegate void UpdateMessage(string txt);
+    public delegate void UpdateListBox(ListBox box, string value, bool remove);
+    public delegate void UpdateCounter(int count);
+
+    public partial class frmServer : Form
+    {
+        Server server;
+
+        public frmServer()
+        {
+            InitializeComponent();
+        }
+
+        private void frmServer_Load(object sender, System.EventArgs e)
+        {
+            tabControl1.SelectTab(tabPage2);
+            tabControl1.TabPages.Remove(tabPage1);
+        }
+
+
+        private void ChangeMessage(string txt)
+        {
+            if (txtMessages.InvokeRequired)
+            {
+                Invoke(new UpdateMessage(ChangeMessage), new object[] { txt });
+            }
+            else
+            {
+
+                txtMessages.Text += txt + "\r\n";
+                txtMessages.SelectionStart = txtMessages.Text.Length;
+                txtMessages.ScrollToCaret();
+            }
+        }
+
+
+
+
+        private void ChangeListBox(ListBox box, string value, bool remove)
+        {
+            if (box.InvokeRequired)
+            {
+                Invoke(new UpdateListBox(ChangeListBox), new object[] { box, value, remove });
+            }
+            else
+            {
+                if (remove)
+                {
+                    box.Items.Remove(value);
+                }
+                else
+                {
+                    box.Items.Add(value);
+                }
+            }
+        }
+
+
+        private void ChangeCounter(int count)
+        {
+            if (tabPage1.InvokeRequired)
+            {
+                Invoke(new UpdateCounter(ChangeCounter), new object[] { count });
+            }
+            else
+            {
+                tabPage1.Text = "Online Users (" + count.ToString() + ")";
+            }
+        }
+
+
+
+        private void server_OnDataReceived(object Sender, ReceivedArguments R)
+        {
+            string[] data = R.ReceivedData.Split('#');
+            string ReceivedChat = R.Name + " : " + R.ReceivedData;
+            ChangeMessage(ReceivedChat);
+
+            
+            
+            if (data[0] == "privMsg")
+            {
+                server.SendTo(data[1], "privMsg#" + R.Name + "#" + data[2]);
+            }
+            else if (data[0] == "userAllMsg")
+            {
+                string yazi = R.ReceivedData;
+                server.BroadCast(yazi);
+            }
+
+            txtMessages.SelectionStart = txtMessages.Text.Length;
+            txtMessages.ScrollToCaret();
+        }
+
+
+        private void server_OnClientDisconnected(object Sender, DisconnectedArguments R)
+        {
+            ChangeListBox(listBox1, R.Name, true);
+            ChangeCounter(server.NumberOfConnections);
+            StringBuilder sendingInfo = new StringBuilder();
+            sendingInfo.Append(server.NumberOfConnections.ToString());
+            for (int i = 0; i < server.NumberOfConnections; i++)
+            {
+                CheckForIllegalCrossThreadCalls = false;
+                string userName = listBox1.Items[i].ToString();
+                sendingInfo.Append("#");
+                sendingInfo.Append(userName);
+            }
+
+            server.BroadCast(sendingInfo.ToString());
+        }
+
+        private void server_OnClientConnected(object Sender, ConnectedArguments R)
+        {
+            //server.BroadCast(R.Name + " Has Connected");
+            ChangeListBox(listBox1, R.Name, false);
+            ChangeCounter(server.NumberOfConnections);
+            StringBuilder sendingInfo = new StringBuilder();
+            sendingInfo.Append(server.NumberOfConnections.ToString());
+            for (int i = 0; i < server.NumberOfConnections; i++)
+            {
+                CheckForIllegalCrossThreadCalls = false;
+                string userName = listBox1.Items[i].ToString();
+                sendingInfo.Append("#");
+                sendingInfo.Append(userName);
+            }
+
+            server.BroadCast(sendingInfo.ToString());
+
+        }
+
+
+
+        
+
+
+
+        private void btnSendAll_Click(object sender, System.EventArgs e)
+        {
+            if (txtChat.Text != "")
+            {
+                string ReceivedChat = txtChat.Text;
+                server.BroadCast("allMsg#" + ReceivedChat);
+                ChangeMessage(ReceivedChat);
+                txtChat.Clear();
+                txtChat.SelectionStart = txtChat.Text.Length;
+                txtChat.ScrollToCaret();
+            }
+            else
+            {
+                MessageBox.Show("Please enter your message!");
+            }
+        }
+
+        private void btnUserKick_Click(object sender, System.EventArgs e)
+        {
+            if (listBox1.SelectedIndex != -1)
+            {
+                server.DisconnectClient((string)listBox1.SelectedItem);
+            }
+            else
+            {
+                MessageBox.Show("Please select user for kick!");
+            }
+        }
+
+        private void frmServer_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            System.Environment.Exit(System.Environment.ExitCode);
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, System.EventArgs e)
+        {
+            listBox1.SelectedIndex = listBox1.SelectedIndex;
+        }
+
+
+        bool isOpen=false;
+        private void btnStart_Click(object sender, System.EventArgs e)
+        {
+            if (isOpen==false)
+            {
+                if (txtIp.Text != "" && txtPort.Text != "")
+                {
+                    server = new Server(txtIp.Text, txtPort.Text);
+                    server.OnClientConnected += new OnConnectedDelegate(server_OnClientConnected);
+                    server.OnClientDisconnected += new OnDisconnectedDelegate(server_OnClientDisconnected);
+                    server.OnDataReceived += new OnReceivedDelegate(server_OnDataReceived);
+                    //server.OnServerError += new OnErrorDelegate(server_OnServerError);
+                    server.Start();
+                    isOpen = true;
+                    btnStart.Text = "Stop";
+                    tabControl1.TabPages.Insert(0, tabPage1);
+                    tabControl1.SelectTab(tabPage1);
+                }
+                else
+                {
+                    MessageBox.Show("Please enter ip and port numbers!");
+
+                }
+            }
+            else
+            {
+                server.Stop();
+                isOpen = false;
+                btnStart.Text = "Start";
+            }
+        }
+
+
+
+
+        private void MesajDegisim(string txt)
+        {
+            if (txtMessages.InvokeRequired)
+            {
+                Invoke(new UpdateMessage(MesajDegisim), new object[] { txt });
+            }
+            else
+            {
+
+                txtMessages.Text += txt + "\r\n";
+                txtMessages.SelectionStart = txtMessages.Text.Length;
+                txtMessages.ScrollToCaret();
+            }
+        }
+
+
+
+        private void button1_Click(object sender, System.EventArgs e)
+        {
+            if (listBox1.SelectedIndex == -1)
+            {
+                MessageBox.Show("Lütfen Kullanıcı Seçiniz..");
+            }
+            else if (txtChat.Text != "")
+            {
+                string yazi = "adminMsg#" + txtChat.Text;
+                server.SendTo((string)listBox1.SelectedItem, yazi);
+                if (listBox1.SelectedItem != null)
+                {
+                    MesajDegisim(yazi);
+                    txtChat.Clear();
+                }
+                txtMessages.SelectionStart = txtMessages.Text.Length;
+                txtMessages.ScrollToCaret();
+
+            }
+            else
+            {
+                MessageBox.Show("Lütfen Mesajınızı Giriniz..");
+            }
+        }
+    }
+}
